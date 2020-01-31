5dd55c0e266fb6df9a2c3a579e57a8bee438ba96
ayushmittal <ayushm@wizni.com>
2020-01-31 22:57:47 +0530

    Initial setup of components and screen

---
+++ App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Navigation from './navigation/index';
+
+//pre cache image to load into application
+const images = [
+  require('./assets/icons/back.png'),
+  require('./assets/icons/plants.png'),
+  require('./assets/icons/seeds.png'),
+  require('./assets/icons/flowers.png'),
+  require('./assets/icons/sprayers.png'),
+  require('./assets/icons/pots.png'),
+  require('./assets/icons/fertilizers.png'),
+  require('./assets/images/plants_1.png'),
+  require('./assets/images/plants_2.png'),
+  require('./assets/images/plants_3.png'),
+  require('./assets/images/explore_1.png'),
+  require('./assets/images/explore_2.png'),
+  require('./assets/images/explore_3.png'),
+  require('./assets/images/explore_4.png'),
+  require('./assets/images/explore_5.png'),
+  require('./assets/images/explore_6.png'),
+  require('./assets/images/illustration_1.png'),
+  require('./assets/images/illustration_2.png'),
+  require('./assets/images/illustration_3.png'),
+  require('./assets/images/avatar.png')
+];
+
+export default class App extends Component {
+  state = {
+    isLoadingComplete: false
+  };
+
+  handleResourceAsync = async () => {
+    //caching all images for better performance on the app
+    const cacheImages = images.map(image =>
+      Asset.fromModule(image).downloadAsync()
+    );
+    return Promise.all(cacheImages);
+  };
+
+  render() {
+    // if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+    //   return (
+    //     <AppLoading
+    //       startAsync={this.handleResourceAsync}
+    //       onError={error => console.warn(error)}
+    //       onFinish={() => this.setState({ isLoadingComplete: true })}
+    //     />
+    //   );
+    // }
+
+    return <Navigation />;
+  }
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/Welcome.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default class Welcome extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
