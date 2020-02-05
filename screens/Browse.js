875e1632b0c980a603899d42e7256ae2ba827b69
ayushmittal <ayushm@wizni.com>
2020-02-05 21:30:16 +0530

    category in browse section

--- screens/Browse.js
+++ screens/Browse.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, ScrollView, Dimensions } from 'react-native';
 
-import { Block, Text, Button } from '../components';
+import { Block, Text, Button, Card, Badge } from '../components';
 import { theme, mocks } from '../constants';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
+const { width } = Dimensions.get('window');
 export default class Browse extends Component {
   state = {
     active: 'Products'
@@ -17,7 +18,7 @@ export default class Browse extends Component {
       <TouchableOpacity
         key={`tab-${tab}`}
         style={[styles.tab, isActive ? styles.active : null]}
-        onPress={() => this.setState({active: tab})}
+        onPress={() => this.setState({ active: tab })}
       >
         <Text size={16} medium gray={!isActive} secondary={isActive}>
           {tab}
@@ -27,7 +28,7 @@ export default class Browse extends Component {
   }
 
   render() {
-    const { profile } = this.props;
+    const { profile, navigation, categories } = this.props;
     const tabs = ['Products', 'Inspirations', 'Shop'];
     return (
       <Block>
@@ -44,13 +45,44 @@ export default class Browse extends Component {
         <Block flex={false} row style={styles.tabs}>
           {tabs.map(tab => this.renderTab(tab))}
         </Block>
+        {/* Card List */}
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          style={{ paddingVertial: theme.sizes.base * 2 }}
+        >
+          <Block flex={false} row space="between" style={styles.categories}>
+            {categories.map(category => (
+              <TouchableOpacity
+                key={category.name}
+                onPress={() => navigation.navigate('Explore', { category })}
+              >
+                <Card center middle shadow style={styles.category}>
+                  <Badge
+                    margin={[0, 0, 15]}
+                    size={50}
+                    color="rgba(41,216,143,0.20)"
+                  >
+                    <Image source={category.image} />
+                  </Badge>
+                  <Text medium height={20}>
+                    {category.name}
+                  </Text>
+                  <Text gray caption>
+                    {category.count} products
+                  </Text>
+                </Card>
+              </TouchableOpacity>
+            ))}
+          </Block>
+        </ScrollView>
       </Block>
     );
   }
 }
 
 Browse.defaultProps = {
-  profile: mocks.profile
+  profile: mocks.profile,
+  categories: mocks.categories
 };
 
 const styles = StyleSheet.create({
@@ -74,5 +106,16 @@ const styles = StyleSheet.create({
   active: {
     borderBottomColor: theme.colors.secondary,
     borderBottomWidth: 3
+  },
+  category: {
+    // this should be dynamic based on screen width
+    minWidth: (width - theme.sizes.padding * 2.4 - theme.sizes.base) / 2,
+    maxWidth: (width - theme.sizes.padding * 2.4 - theme.sizes.base) / 2,
+    maxHeight: (width - theme.sizes.padding * 2.4 - theme.sizes.base) / 2
+  },
+  categories: {
+    flexWrap: 'wrap',
+    paddingHorizontal: theme.sizes.base * 2,
+    marginBottom: theme.sizes.base * 3.5
   }
 });
